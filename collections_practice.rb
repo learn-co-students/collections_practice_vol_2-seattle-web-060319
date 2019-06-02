9e5d03879436d401ff0df2682db5d037cc18e229
Justin <sumbhodi1618@gmail.com>
2019-06-02 22:07:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,4 +7,23 @@ def contain_a(array)
   array.select {|word| word.include?("a")}
 end
 
-def
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.reject {|index| index.class != String}
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
+end
+
+def merge_data(keys, data)
+    
+end
