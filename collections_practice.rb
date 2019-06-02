fc65143520b7f4143862b50414b574a447ec034d
Justin <sumbhodi1618@gmail.com>
2019-06-02 18:58:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,10 @@
-# your code goes here
+require 'pry'
+def begins_with_r(array)
+  array.all? {|word| word.chr == "r"}
+end
+
+def contain_a(array)
+  array.select {|word| word.include?("a")}
+end
+
+def
